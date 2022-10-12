ada30abe0689c5f24055c42cfed20c4296f2e61b
spacehaz <spacehaz@gmail.com>
2022-10-12 17:06:11 +0300

    added fix

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -16,7 +16,7 @@ module.exports = {
       ref: `${GIT_REF}`,
       repo: `${GIT_REPOSITORY}`,
       path: `${DESTINATION_PATH}`,
-      'pre-deploy-local': `scp -Cr ./env ${SSH_USERNAME}@${SSH_HOSTMACHINE}:${DESTINATION_PATH}/backend`,
+      'pre-deploy-local': `scp -Cr .env ${SSH_USERNAME}@${SSH_HOSTMACHINE}:${DESTINATION_PATH}/backend`,
       'post-deploy': 'npm i && npm run build && pm2 startOrRestart ecosystem.config.js --env production',
     },
   },
